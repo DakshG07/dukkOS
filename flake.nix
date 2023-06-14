cd7f3c988cea56e4269859dd592c547afb4781e1
Daksh Gupta <acedaksh07@gmail.com>
2023-06-14 18:03:02 -0400

    feat: add git

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs, home-manager, ... }@attrs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = attrs;
+        modules = [ 
+	  ./config/configuration.nix
+	  home-manager.nixosModules.home-manager
+	  {
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+	    home-manager.users.dukk = import ./home/home.nix;
+	  }
+	];
+      };
+    };
+}
